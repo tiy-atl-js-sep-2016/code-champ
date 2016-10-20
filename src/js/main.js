deefa5d45acab35d9c35b073a968ea56eaa021f0
Brit Butler <redline6561@gmail.com>
2016-10-20 11:46:13 -0400

    Run data through templates and add to page.

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@ import $ from 'jquery';
 import _ from 'lodash';
 import { getUser, getRepoStats } from "./github";
 
+var resultsBox = $(".results");
+
 function extractUserInfo (user) {
   var contributions = { added: 0, deleted: 0, commits: 0 };
 
@@ -18,11 +20,39 @@ function extractUserInfo (user) {
   };
 };
 
+function rankingTemplate (userData) {
+  // For an Individual User
+  return `
+     <li class="ranking">
+       <p>
+         <a href="https://github.com/users/${userData.author}">${userData.author}</a>
+         has made ${userData.total} commits, adding ${userData.contributions.added}
+         lines of code, and deleted ${userData.contributions.deleted} lines of code.
+       </p>
+     </li>
+    `;
+};
+
+function projectTemplate (rankingsHtml) {
+  return `
+    <h2 class="project">Hacker Rankings</h2>
+    <ol class="rankings">
+      ${rankingsHtml.join("")}
+    </ol>
+    `;
+};
+
 function generateRankings (data) {
   var sorted = _.orderBy(data, ['total'], ['desc']);
   var top10 = _.take(sorted, 10);
   var totals = top10.map(extractUserInfo);
   return totals;
+};
+
+function addTotalsToPage (totals) {
+  var rankings = totals.map(rankingTemplate);
+  var html = projectTemplate(rankings);
+  resultsBox.html(html);
 };
 
 function getRankings (event) {
@@ -32,7 +62,7 @@ function getRankings (event) {
 
   var req = getRepoStats(userName, repoName);
   var totals = req.then(generateRankings);
-  totals.then(console.log);
+  totals.then(addTotalsToPage);
 
   // console.log("THIS HAPPENS FIRST");
   // foo();
