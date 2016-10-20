42dd2db76074e9a1c067574ccab0b78d65757a99
Brit Butler <redline6561@gmail.com>
2016-10-20 10:17:59 -0400

    Sort the Github data and keep the top 10.

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,33 @@
 import $ from 'jquery';
 import { getUser, getRepoStats } from "./github";
 
-function printData (data) {
-  console.log(data);
-}
+function sortByTotal (a, b) {
+  if (a.total > b.total) {
+    return -1;
+  } else if (a.total < b.total) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
+
+function generateRankings (data) {
+  var sortedData = data.sort(sortByTotal);
+  var top10 = sortedData.slice(0, 10);
+  console.log(top10);
+};
 
 function getRankings (event) {
   event.preventDefault();
-  var req = getRepoStats("jquery", "jquery");
-  req.then(printData);
-  console.log("The request is away!");
+  var userName = $("#user-name").val();
+  var repoName = $("#repo-name").val();
+
+  var req = getRepoStats(userName, repoName);
+  req.then(generateRankings);
 }
 
 $("#submit-button").click(getRankings);
+
+// console.log($("#user-name"));
+
+// getRepoStats("jquery", "jquery").then(console.log);
