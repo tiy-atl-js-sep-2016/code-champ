21ec979b82e9bb0de01f0324f792d10d7202f72b
Brit Butler <redline6561@gmail.com>
2016-10-20 11:31:04 -0400

    Talk a lot about promises.

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,28 @@
 import $ from 'jquery';
+import _ from 'lodash';
 import { getUser, getRepoStats } from "./github";
 
-function sortByTotal (a, b) {
-  if (a.total > b.total) {
-    return -1;
-  } else if (a.total < b.total) {
-    return 1;
-  } else {
-    return 0;
-  }
+function extractUserInfo (user) {
+  var contributions = { added: 0, deleted: 0, commits: 0 };
+
+  var answers = user.weeks.forEach(function (week) {
+    contributions.added += week.a;
+    contributions.deleted += week.d;
+    contributions.commits += week.c;
+  });
+
+  return {
+    author: user.author.login,
+    total: user.total,
+    contributions: contributions
+  };
 };
 
 function generateRankings (data) {
-  var sortedData = data.sort(sortByTotal);
-  var top10 = sortedData.slice(0, 10);
-  console.log(top10);
+  var sorted = _.orderBy(data, ['total'], ['desc']);
+  var top10 = _.take(sorted, 10);
+  var totals = top10.map(extractUserInfo);
+  return totals;
 };
 
 function getRankings (event) {
@@ -23,8 +31,16 @@ function getRankings (event) {
   var repoName = $("#repo-name").val();
 
   var req = getRepoStats(userName, repoName);
-  req.then(generateRankings);
+  var totals = req.then(generateRankings);
+  totals.then(console.log);
+
+  // console.log("THIS HAPPENS FIRST");
+  // foo();
 }
+
+function foo () {
+  console.log("we've moved on to new tasks");
+};
 
 $("#submit-button").click(getRankings);
 
